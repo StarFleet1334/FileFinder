846ae2da97641e7830864c04aeaf28b22ae73450
IliaLataria01 <latariailia6@gmail.com>
2024-02-10 02:13:10 +0400

    Another was to add data into a file

--- file_finder.go
+++ file_finder.go
@@ -6,43 +6,27 @@ import (
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) == 0 {
-		fmt.Println("Provide a directory")
+
+	file, error := os.OpenFile("empty_second.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if error != nil {
+		fmt.Println("Something was wrong")
 		return
 	}
 
-	files, err := os.ReadDir(args[0])
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	data := []byte("Hello World!\n")
-	err = os.WriteFile("empty_second.txt", data, 0644)
-
-	if err != nil {
-		fmt.Println("Error writing to a file: ", err)
-		return
-	}
-
-	fmt.Println("Data written successfully to empty_second")
-
-	var names []string
-
-	for _, file := range files {
-		info, err := file.Info() // Get FileInfo for each file/directory
+	defer func(file *os.File) {
+		err := file.Close()
 		if err != nil {
-			fmt.Println("Error retrieving file info:", err)
-			continue
-		}
 
-		// Check if the file size is 0 (file is empty)
-		if info.Size() == 0 {
-			names = append(names, info.Name())
 		}
+	}(file)
+
+	_, err := file.WriteString("Hello, My Good Old Friend\n")
+	if err != nil {
+		fmt.Printf("There was an error writing to a file named: %s\n", file.Name())
+		return
 	}
 
-	fmt.Println(names)
+	fmt.Printf("Data was successfully writtent into file named: %s\n", file.Name())
 
 }
