db0ab5f013bcefb704471472147c2767736d0ad1
IliaLataria01 <latariailia6@gmail.com>
2024-02-10 02:07:22 +0400

    Write to a file

--- file_finder.go
+++ file_finder.go
@@ -18,6 +18,18 @@ func main() {
 		return
 	}
 
+	data := []byte("Hello World!\n")
+	err = os.WriteFile("empty_second.txt", data, 0644)
+
+	if err != nil {
+		fmt.Println("Error writing to a file: ", err)
+		return
+	}
+
+	fmt.Println("Data written successfully to empty_second")
+
+	var names []string
+
 	for _, file := range files {
 		info, err := file.Info() // Get FileInfo for each file/directory
 		if err != nil {
@@ -27,8 +39,10 @@ func main() {
 
 		// Check if the file size is 0 (file is empty)
 		if info.Size() == 0 {
-			fmt.Println(file.Name()) // Print the name of the empty file
+			names = append(names, info.Name())
 		}
 	}
+
+	fmt.Println(names)
 
 }
