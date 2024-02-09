b898c7444161ec74f9009a72aa90f77e7eca8a5b
IliaLataria01 <latariailia6@gmail.com>
2024-02-09 21:52:19 +0400

    Fetch the files functionality, in this case fetches empty files

--- file_finder.go
+++ file_finder.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Provide a directory")
+		return
+	}
+
+	files, err := os.ReadDir(args[0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, file := range files {
+		info, err := file.Info() // Get FileInfo for each file/directory
+		if err != nil {
+			fmt.Println("Error retrieving file info:", err)
+			continue
+		}
+
+		// Check if the file size is 0 (file is empty)
+		if info.Size() == 0 {
+			fmt.Println(file.Name()) // Print the name of the empty file
+		}
+	}
 
 }
